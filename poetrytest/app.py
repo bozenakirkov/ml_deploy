20cde631bc8e4f47c2bafb3b1368157c067f1390
Bozena <bozena.kirkov@o2.pl>
2023-02-26 14:24:08 +0200

    added swagger

--- poetrytest/app.py
+++ poetrytest/app.py
@@ -1,21 +1,40 @@
 from flask import Flask, request
 from poetrytest.inferrer import Inferrer
+from flasgger import Swagger
 import os
 
-# APP_ROOT = os.getenv('APP_ROOT', '/infer')
-
 app = Flask(__name__)
+swagger = Swagger(app)
+
+upload_folder = "../tmp"
+if not os.path.exists(upload_folder):
+    os.mkdir(upload_folder)
+app.config['uploadFolder'] = upload_folder
 
 inferrer = Inferrer()
 
 
 @app.route("/infer", methods=["POST"])
 def infer():
-    data = request.json
-    img_path_npz = data.get("input_file")
-    return inferrer.infer(img_path_npz)
+    """Predict digits
+        ---
+        parameters:
+        - name: input_file
+          in: formData
+          type: file
+          required: true
+          description: Select NPZ file with tensor data.
+        responses:
+          default:
+            description: Predictions for [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] digits.
+        """
+    input_file = request.files.get("input_file")
+    input_file_path = os.path.join(app.config['uploadFolder'],
+                                   input_file.filename)
+    input_file.save(input_file_path)
+    return inferrer.infer(input_file_path)
 
 
 if __name__ == '__main__':
-    # host = HOST, port = PORT_NUMBER
     app.run()
+
